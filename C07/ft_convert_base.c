b87fa97126e6738d13ce68c09626596606745fe4
SYKIM <kimsbs1225@naver.com>
2021-03-09 17:51:12 +0900

    first

---
+++ C07/ft_convert_base.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <unistd.h>
+
+char *ft_convert_base2(int sol, char *base_to);
+
+int base_check(char *base)
+{
+	int i;
+	int j;
+
+	if(!base)
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		if(base[i] == '+' || base[i] == '-')
+			return (0);
+		j = 0;
+		while(j < i)
+		{
+			if(base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int find_base(char c, char *base)
+{
+	int i;
+	
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return i;
+		i++;
+	}
+	return (-1);
+}
+int possible(char c, char *base)
+{
+	if (c == '+' || c == '-')
+		return (1);
+	else if (find_base(c, base) != -1)
+		return (2);
+	else if (c == ' ' || c == '	' || c == '\n')
+		return (1);
+	else
+		return (0);
+}
+int make_int(char *nbr, int cnt, char *base, int b_len)
+{
+	int len;
+	int i;
+	int tmp;
+	int sol;
+	int tmp2;
+
+	len = 0;
+	sol = 0;
+	while (possible(nbr[len], base) == 2)
+		len++;
+		i = -1;
+	while (++i < len)
+	{
+		tmp2 = len - i;													tmp = 1;
+		if (cnt % 2 != 0)
+			tmp = -1;
+		tmp *= find_base(nbr[i], base);
+		while (--tmp2)
+			tmp *= b_len;
+		sol += tmp;
+	}
+	return (sol);
+}
+
+char *ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	int cnt;
+	int b_len;
+	int sol;
+	char *tmp;
+
+	cnt = 0;
+	b_len = 0;
+	if(!base_check(base_from) || !base_check(base_to))
+		return (0);
+	while (base_from[b_len])
+		b_len++;
+	while (*nbr)
+	{
+		if (possible(*nbr, base_from))
+		{
+			if (*nbr == '-')
+				cnt++;
+			else if (find_base(*nbr, base_from) != -1)
+			{
+				sol = make_int(nbr, cnt, base_from, b_len);
+				tmp = ft_convert_base2(sol, base_to);
+				return (tmp);
+			}
+		}
+		else
+			break;													nbr++;
+	}
+	return (0);
+}
