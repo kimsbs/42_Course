379047145cd5c54ae8ad80425f8a1d1a650f29fd
Seungyki <69835873+kimsbs@users.noreply.github.com>
2021-05-14 15:17:33 +0900

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -5,114 +5,91 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: seungyki <seungyki@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/11 14:51:46 by seungyki          #+#    #+#             */
-/*   Updated: 2021/05/11 17:39:12 by seungyki         ###   ########.fr       */
+/*   Created: 2021/05/11 09:48:11 by seungyki          #+#    #+#             */
+/*   Updated: 2021/05/14 15:09:21 by seungyki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-int		is_nextline(t_list *header)
+int		is_next_line(char *s)
 {
-	int move;
-  int sol;
-  if(!header)
-    return (0);
-	sol = 0;
-  while(header)
-  {
-    move = 0;
-	  while (header->content[move])
-	  {
-		  if (header->content[move] == '\n')
-      {
-		  	return (sol);
-     }
-	  	move++;
-      sol++;
-  	}
-    header = header->next;
-  }
+	int		move;
+
+	move = 0;
+	while (s[move])
+	{
+		if (s[move] == '\n')
+			return (move);
+		move++;
+	}
 	return (-1);
 }
 
-void free_list(t_list *header)
+int		split_and_done(char **sol, char **line, int next_line_whenence,
+		char *buf)
 {
-  if(!header)
-    return ;
-  free(header->content);
-  free(header);
-}
+	int		left_len;
+	char	*left;
 
-t_list	*save_buffer(t_list *header, size_t dummy, char **line)
-{
-	char	*tmp;
-	int		move;
-	int		tmp_move;
-  t_list  *dump;
-
-	tmp = (char *)malloc(sizeof(char) * (dummy + 1));
-	tmp_move = 0;
-	while (header)
+	if (buf)
+		free(buf);
+	(*sol)[next_line_whenence] = '\0';
+	left_len = ft_strlen(*sol + next_line_whenence + 1);
+	*line = ft_strdup(*sol);
+	if (left_len == 0)
 	{
-		move = -1;
-		while (header->content[++move])
-		{
-      if(header->content[move] == '\n')
-      {
-        tmp[tmp_move] = '\0';
-        *line = tmp;
-        header->content = &(header->content[++move]);
-        return (header);
-      }
-			tmp[tmp_move] = header->content[move];
-			tmp_move++;
-		}
-    dump = header;
-		header = header->next;
-   // free_list(dump);
+		free(*sol);
+		*sol = 0;
+		return (1);
 	}
-  return (header);
+	left = ft_strdup(*sol + next_line_whenence + 1);
+	free(*sol);
+	if (!left)
+		return (-1);
+	*sol = left;
+	return (1);
 }
 
-void      print_list(t_list *list)
+int		while_is_over(char **sol, char **line, int rd_len)
 {
-  char *srr;
-  while(list)
-  {
-    srr = list->content;
-    printf("%s", srr);
-    list = list->next;
-  }
+	int		next_line_whenece;
+
+	if (rd_len == 0)
+	{
+		if (*sol && (next_line_whenece = is_next_line(*sol)) >= 0)
+			return (split_and_done(sol, line, next_line_whenece, 0));
+		else if (*sol)
+		{
+			*line = *sol;
+			*sol = NULL;
+			return (0);
+		}
+		*line = ft_strdup("");
+		return (0);
+	}
+	return (-1);
 }
 
-int				get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
-	int		rd_len;
-	int		tmp;
-	char	*buf;
-	static  t_list	*header;
+	char			*buf;
+	static	char	*sol[OPEN_MAX];
+	int				rd_len;
+	int				next_line_whenece;
 
-  buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (fd <= 0 || !line || BUFFER_SIZE <= 0)
+		return (-1);
+	if (!(buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
 	while ((rd_len = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[rd_len] = '\0';
-		ft_lstadd_back(&header, buf, rd_len);
-		if ((tmp = is_nextline(header)) >= 0)
-		{
-			header = save_buffer(header, tmp, line);
-			return (1);
-		}
+		sol[fd] = ft_strjoin(sol[fd], buf);
+		if ((next_line_whenece = is_next_line(sol[fd])) >= 0)
+			return (split_and_done(&sol[fd], line, next_line_whenece, buf));
 	}
-	if (rd_len == 0)
-  {
-      if ((tmp = is_nextline(header)) >= 0)
-		  {
-			  header = save_buffer(header, tmp, line);
-			  return (1);
-		  }
-		return (0);
-  }
-	else
-		return (-1);
+	free(buf);
+	return (while_is_over(&sol[fd], line, rd_len));
 }
