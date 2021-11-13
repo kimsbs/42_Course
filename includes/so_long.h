ab7ba26f0494a664ae9b8088997fabb7c9ce4a12
kimsbs <kimsbs1225@naver.com>
2021-11-13 18:35:37 +0900

    print_map

--- includes/so_long.h
+++ includes/so_long.h
@@ -9,7 +9,37 @@
 # include <fcntl.h>
 # include <mlx.h>
 
-typedef struct s_game{
+# define TILE_SIZE 71
+
+typedef struct s_img
+{
+  void  *ptr;
+  int   w;
+  int   h;
+} t_img;
+
+typedef struct s_links
+{
+  t_img tree;
+  t_img tile;
+  t_img coin;
+  t_img exit;
+  t_img pu0;
+  t_img pu1;
+  t_img pu2;
+  t_img pd0;
+  t_img pd1;
+  t_img pd2;
+  t_img pl0;
+  t_img pl1;
+  t_img pr0;
+  t_img pr1;
+} t_links;
+
+typedef struct s_game
+{
+    void   *mlx;
+    void    *win;
     int     p[2];
     char    **map;
     int     player_cnt;
@@ -17,6 +47,8 @@ typedef struct s_game{
     int     exit_cnt;
     int     height;
     int     width;
+    t_img   dummy;
+    t_links link;
 }t_game;
 
 typedef struct s_data
@@ -27,12 +59,6 @@ typedef struct s_data
   int line_length;
   int endian;
 } t_data;
-
-typedef struct s_ptr
-{
-  void *mlx;
-  void *win;
-} t_ptr;
 
 void    exit_handler(char *s1);
 void	ft_putchar(char c);
--- srcs/func_mlx.c
+++ srcs/func_mlx.c
@@ -1,10 +1,10 @@
 #include "../includes/so_long.h"
 
-int deal_key(int key, t_ptr *mlx)
+int deal_key(int key, t_game *game)
 {
   if (key == 53)
   {
-    mlx_destroy_window(mlx->mlx, mlx->win);
+    mlx_destroy_window(game->mlx, game->win);
     exit(0);
   }
   else if (key == 13 || key == 126)
@@ -20,39 +20,81 @@ int deal_key(int key, t_ptr *mlx)
   return (0);
 }
 
-int exit_with_mouse(t_ptr *mlx)
+int exit_with_mouse(t_game *game)
 {
-  mlx_destroy_window(mlx->mlx, mlx->win);
+  mlx_destroy_window(game->mlx, game->win);
   exit(0);
   return (0);
 }
 
-void my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	ft_put_img64(t_game *game, void *img_ptr, int x, int y)
 {
-  char *dst;
-
-  dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-  *(unsigned int *)dst = color;
+	mlx_put_image_to_window(game->mlx, game->win, img_ptr, y * 40, x * 40);
 }
 
-void  using_mlx(t_game *map)
+void *get_xpm_ptr(t_game *game, char *str)
 {
-  t_ptr  mlx;
-  t_data img;
+  void *img;
+  img = mlx_xpm_file_to_image(game->mlx, str, &(game->dummy.w), &(game->dummy.h)); 
+  if(!img)
+    exit_handler("xpm error\n");
+  return (img);
+}
 
-  mlx.mlx = mlx_init();
-  mlx.win = mlx_new_window(mlx.mlx, map->width * 40, map->height * 40, "So_long");
-  img.img = mlx_new_image(mlx.mlx, map->width * 40, map->height * 40);
-  img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-  for (int i = 0; i < map->width * 40; i++)
+void  img_init(t_game *game)
+{
+  game->link.coin.ptr = get_xpm_ptr(game, "./img/coin.xpm");
+  game->link.exit.ptr = get_xpm_ptr(game, "./img/exit.xpm");
+  game->link.tile.ptr = get_xpm_ptr(game, "./img/tile.xpm");
+  game->link.tree.ptr = get_xpm_ptr(game, "./img/tree.xpm");
+  game->link.pd0.ptr = get_xpm_ptr(game, "./img/pd0.xpm");
+  game->link.pd1.ptr = get_xpm_ptr(game, "./img/pd1.xpm");
+  game->link.pd2.ptr = get_xpm_ptr(game, "./img/pd2.xpm");
+  game->link.pu0.ptr = get_xpm_ptr(game, "./img/pu0.xpm");
+  game->link.pu1.ptr = get_xpm_ptr(game, "./img/pu1.xpm");
+  game->link.pu2.ptr = get_xpm_ptr(game, "./img/pu2.xpm");
+  game->link.pl0.ptr = get_xpm_ptr(game, "./img/pl0.xpm");
+  game->link.pl1.ptr = get_xpm_ptr(game, "./img/pl1.xpm");
+  game->link.pr0.ptr = get_xpm_ptr(game, "./img/pr0.xpm");
+  game->link.pr1.ptr = get_xpm_ptr(game, "./img/pr1.xpm");
+}
+
+void  put_entity(t_game *game, int x, int y)
+{
+  if(game->map[y][x] == '1')
   {
-    for (int j = 0; j < map->height * 40; j++)
+    mlx_put_image_to_window(game->mlx, game->win, game->link.tree.ptr, x * TILE_SIZE, y * TILE_SIZE);   
+  }  
+  else
+  {
+    mlx_put_image_to_window(game->mlx, game->win, game->link.tile.ptr, x * TILE_SIZE, y * TILE_SIZE);  
+  }
+}
+
+void  print_map(t_game *game)
+{
+  int x;
+  int y;
+
+  y = -1;
+  while(++y < game->height)
+  {
+    x = -1;
+    while(++x < game->width)
     {
-      mlx_pixel_put(mlx.mlx, mlx.win, i, j, 0x00FFFFFF);
+      put_entity(game, x, y);
     }
   }
-  //mlx_put_image_to_window(mlx.mlx, mlx.win, img.img, 0, 0);
-  mlx_key_hook(mlx.win, deal_key, &mlx);
-  mlx_hook(mlx.win, 17, 0, exit_with_mouse, &mlx);
-  mlx_loop(mlx.mlx);
+}
+
+void  using_mlx(t_game *game)
+{
+
+  game->mlx = mlx_init();
+  game->win = mlx_new_window(game->mlx, game->width * TILE_SIZE, game->height * TILE_SIZE, "So_long");
+  img_init(game);
+  print_map(game);
+  mlx_key_hook(game->win, deal_key, game);
+  mlx_hook(game->win, 17, 0, exit_with_mouse, game);
+  mlx_loop(game->mlx);
 }
