8c28c7b145e18bae042abcc4b30ea85b80d9515b
Seungyeon Kim <seungyki@c2r3s5.42seoul.kr>
2021-11-16 10:36:54 +0900

    norm_fix

--- includes/so_long.h
+++ includes/so_long.h
@@ -13,64 +13,56 @@
 
 typedef struct s_img
 {
-  void  *ptr;
-  int   w;
-  int   h;
-} t_img;
+	void	*ptr;
+	int		w;
+	int		h;
+}	t_img;
 
 typedef struct s_links
 {
-  t_img tree;
-  t_img tile;
-  t_img coin;
-  t_img exit;
-  t_img pu[3];
-  t_img pd[3];
-  t_img pl[2];
-  t_img pr[2];
-} t_links;
+	t_img	tree;
+	t_img	tile;
+	t_img	coin;
+	t_img	exit;
+	t_img	pu[3];
+	t_img	pd[3];
+	t_img	pl[2];
+	t_img	pr[2];
+}	t_links;
 
 typedef struct s_game
 {
-    void    *mlx;
-    void    *win;
-    int     player_x;
-    int     player_y;
-    char    **map;
-    int     player_cnt;
-    int     coin_cnt;
-    int     exit_cnt;
-    int     height;
-    int     width;
-    int     move;
-    t_img   player;
-    t_img   dummy;
-    t_links link;
-}t_game;
+	void	*mlx;
+	void	*win;
+	int		player_x;
+	int		player_y;
+	char	**map;
+	int		player_cnt;
+	int		coin_cnt;
+	int		exit_cnt;
+	int		height;
+	int		width;
+	int		move;
+	t_img	player;
+	t_img	dummy;
+	t_links	link;
+}	t_game;
 
-typedef struct s_data
-{
-  void *img;
-  char *addr;
-  int bits_per_pixel;
-  int line_length;
-  int endian;
-} t_data;
+void	exit_handler(char *s1);
+void	ft_putchar(char c);
+void	ft_putstr(char *s);
+int		ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+int		ft_strlcpy(char *dst, const char *src, int dstsize);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	**ft_split(char const *s, char c);
+void	read_map(int fd, t_game *game);
+void	using_mlx(t_game *map);
+void	print_map(t_game *game, int flag);
+void	move_to_west(t_game *game);
+void	move_to_east(t_game *game);
+void	move_to_south(t_game *game);
+void	move_to_north(t_game *game);
+void	img_init(t_game *game);
 
-void    exit_handler(char *s1);
-void	  ft_putchar(char c);
-void	  ft_putstr(char *s);
-int	    ft_strlen(const char *s);
-char	  *ft_strdup(const char *s1);
-int 	  ft_strlcpy(char *dst, const char *src, int dstsize);
-char   	*ft_strjoin(char const *s1, char const *s2);
-char    **ft_split(char const *s, char c);
-void    read_map(int fd, t_game *game);
-void    using_mlx(t_game *map);
-void    print_map(t_game *game, int flag);
-void    move_to_west(t_game *game);
-void    move_to_east(t_game *game);
-void    move_to_south(t_game *game);
-void    move_to_north(t_game *game);
-void    img_init(t_game *game);
 #endif
