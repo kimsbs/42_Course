7a5a2412388de4c251196cb6e5cf69a5851d5efd
kimsbs <kimsbs1225@naver.com>
2021-12-18 14:49:18 +0900

    rm semaphore

--- sem/srcs/error.c
+++
@@ -1,9 +0,0 @@
-#include "philo.h"
-
-void    argc_error()
-{
-    printf("input 4 or 5 arguments\n");
-    printf("1: [number_of_philosophers]\n2: [time_to_die]\n");
-    printf("3: [time_to_eat]\n4: [time_to_sleep]\n");
-    printf("5: [number_of_times_each_philosopher_must_eat : optional]\n");
-}
